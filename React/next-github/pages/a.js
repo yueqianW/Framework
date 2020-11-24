c13f61516620aceca47571beb90d50b082064b39
yueqianW <ames.yueqian@hotmail.com>
2020-11-24 08:43:18 +0800

    add: vue-mooc

--- React/next-github/pages/a.js
+++ React/next-github/pages/a.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Comp from '../components/comp'
 
 export default () => <Comp>666</Comp>
--- React/next-github/pages/index.js
+++ React/next-github/pages/index.js
@@ -1,2 +1,3 @@
+import React from 'react';
 
 export default () => <span>111</span>;
