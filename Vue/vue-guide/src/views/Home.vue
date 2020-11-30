8fe0cf23dbc12f39ad0365381d4fdaeed3eccac6
yueqianW <ames.yueqian@hotmail.com>
2020-11-30 13:52:22 +0800

    add: vue-begin

---
+++ Vue/vue-guide/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="home">{{ msg }}</div>
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: 'hello view'
+    }
+  },
+  components: {},
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.left {
+
+}
+
+.right {
+}
+</style>
