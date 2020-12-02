34f7cf8fc1f71fb9b50907a7d6cfca7251a806d5
yueqianW <ames.yueqian@hotmail.com>
2020-12-02 21:11:50 +0800

    add: koa

--- Vue/vue-mooc/src/index.js
+++ Vue/vue-mooc/src/index.js
@@ -1,9 +1,16 @@
-const koa = require('koa')
-const path = require('path')
+// const koa = require('koa')
+// const path = require('path')
+// const helmet = require('koa-helmet')
+// const statics = require('koa-static')
+// const router = require('./routes/routes')
+
+import koa from 'koa';
+import path from 'path';
+import helmet from 'koa-helmet';
+import statics from 'koa-static';
+import router from './routes/routes';
+
 const app = new koa()
-const helmet = require('koa-helmet')
-const statics = require('koa-static')
-const router = require('./routes/routes')
 
 app.use(helmet())
 app.use(statics(path.join(__dirname, '../public')))
