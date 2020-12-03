8561e2d212ab94a6ff91fd09707fed9f8e5f576d
yueqianW <ames.yueqian@hotmail.com>
2020-12-03 17:50:37 +0800

    add: webpack

---
+++ webpack/demo/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './dist')
+  },
+  module: {
+    rules: [{
+      test: /\.(scss|sass)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  }
+
+} 
