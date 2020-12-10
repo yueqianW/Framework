ca9e064b84317ce3731aee3bba4163754b718629
yueqianW <ames.yueqian@hotmail.com>
2020-12-10 18:11:22 +0800

    del: pro

--- React/blog-system/admin/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './pages/main';
-
-ReactDOM.render(<Main />, document.getElementById('root'));
-
--- React/next-github/pages/a.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Comp from '../components/comp'
-
-export default () => <Comp>666</Comp>
