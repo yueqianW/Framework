d22ca4aa9f690c0b1916618151f89f3578fed3ed
yueqianW <ames.yueqian@hotmail.com>
2020-12-11 17:30:44 +0800

    add: next less

--- React/nextjs-github/pages/index.js
+++ React/nextjs-github/pages/index.js
@@ -24,10 +24,6 @@ export default function Home() {
         <h1 className="title">
           Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <h1 className="title">
-          Read <Link href="/posts/first-post"><a>this page!</a></Link>
-        </h1>
-
         <p className="description">
           Get started by editing <code>pages/index.jsx</code>
         </p>
