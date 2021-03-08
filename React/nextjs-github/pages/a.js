1dc92129a964d6354a676e8e0bfe0b6f3ce2c5ad
yueqianW <ames.yueqian@hotmail.com>
2021-03-08 10:50:44 +0800

    delete react、vue

--- React/nextjs-github/pages/a.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default () => <div>666</div>
--- React/nextjs-github/pages/photo/b.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { withRouter } from "next/router";
-
-const A = ({ router }) => {
-  return (
-    <div>111 {router.query.name}</div>
-  )
-}
-export default withRouter(A)
