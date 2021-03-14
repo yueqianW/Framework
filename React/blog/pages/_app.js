d9a6b0d3c122b3259d3ef0e54fefd82e4e6d4b0b
yueqianW <ames.yueqian@hotmail.com>
2021-03-14 14:52:20 +0800

    blog setup

--- React/blog/pages/_app.js
+++ React/blog/pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import App from 'next/app'
+import 'antd/dist/antd.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
-export default MyApp
+// export default MyApp
+export default App
