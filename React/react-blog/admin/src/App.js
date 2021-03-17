e216549ed9f2a0c9b30763a831c864ea6bb88aef
yueqianW <ames.yueqian@hotmail.com>
2021-03-17 16:24:11 +0800

    add admin

---
+++ React/react-blog/admin/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div> <Button>hello</Button></div >
+    )
+  }
+}
+
+export default App
---
+++ React/react-blog/admin/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+class Login extends Component {
+  render() {
+    return (
+      <div> <Button>Login</Button></div >
+    )
+  }
+}
+
+export default Login
