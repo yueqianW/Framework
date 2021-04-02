b6781087c31e558237b52ab441e2fc4c65cdaa99
yueqianW <ames.yueqian@hotmail.com>
2021-04-02 23:30:33 +0800

    add style

--- React/react-blog/admin/src/pages/layout/layout.js
+++ React/react-blog/admin/src/pages/layout/layout.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
-import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+import React, { Component } from "react";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
 
-const { Header, Content, Footer, Sider } = Layout
-const { SubMenu } = Menu
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 class SiderDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
 
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
   render() {
-    return {
-    }
+    return (
+      <Layout>
+        <Sider collapsible>
+          <Menu theme="dark">
+            <Menu.Item key="1">
+              <Icon />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+      </Layout>
+    );
   }
 }
