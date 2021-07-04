d4239d178b7b5b0c0cc363df01c2a3e2cb9ef93f
yueqianW <ames.yueqian@hotmail.com>
2021-07-04 15:04:48 +0800

    add webpack

---
+++ webpack/demo2/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
